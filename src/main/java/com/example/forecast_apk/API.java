44b33d9f3278205158c18efa9f9b578dfe39b553
userABipul-Bimali <bimalibipul@gmail.com>
2023-11-28 16:37:04 -0500

    backend processing is complete

--- src/main/java/com/example/forecast_apk/API.java
+++ src/main/java/com/example/forecast_apk/API.java
@@ -11,6 +11,7 @@ import com.google.gson.JsonParser;
 @SuppressWarnings("ALL")
 public class API {
     String host = "https://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&product=astro&output=json";
+    int timepoint;
     String[ ] Dataseries;
     String[ ]  Timepoint={"a","b","c","d","e","f","g","h"};
 //    String[ ]  a;
@@ -49,43 +50,131 @@ public class API {
     private void JSONSeperation(StringBuilder builder){
 
         String str = builder+"";
-        System.out.println(builder);
+//        System.out.println(builder);
         JsonObject obj = JsonParser.parseString(str).getAsJsonObject();
         JsonArray  dataseries = obj.get("dataseries").getAsJsonArray();
+        forecastData a = new forecastData();
+        forecastData b = new forecastData();
+        forecastData c = new forecastData();
+        forecastData d = new forecastData();
+        forecastData e = new forecastData();
+        forecastData f = new forecastData();
+        forecastData g = new forecastData();
+        forecastData h = new forecastData();
         for (JsonElement dataset: dataseries ) {
-            JsonObject data = JsonParser.parseString(dataset+"").getAsJsonObject();
+            JsonObject data = JsonParser.parseString(dataset + "").getAsJsonObject();
             String produc = data.get("timepoint").getAsString();
-            System.out.println("hieeee");
-            System.out.println("Product: " + produc);
-            for (int  i = 0 ; i < 10 ; i++){
-                switch (i){
-                    case 1:{
-                        forecastData a = new forecastData();
-                    }
-                    case 2:{
-                        forecastData b = new forecastData();
-                    }
-                    case 3:{
-                        forecastData c = new forecastData();
-                    }
-                    case 4:{
-                        forecastData d = new forecastData();
-                    }
-                    case 5:{
-                        forecastData e = new forecastData();
-                    }
-                    case 6:{
-                        forecastData f = new forecastData();
-                    }
-                    case 7:{
-                        forecastData g = new forecastData();
-                    }
-                    case 8:{
-                        forecastData h = new forecastData();
-                    }
+//            System.out.println("hieeee");
+//            System.out.println("Product: " + produc);
+            timepoint = data.get("timepoint").getAsInt();
+            String r = data.get("prec_type").getAsString();
+            System.out.println(timepoint);
+
+            switch (timepoint){
+                case 3:{
+                    JsonObject wind = data.get("wind10m").getAsJsonObject();
+                    System.out.println("CLASS:: "+data.get("wind10m").getClass());
+//                    System.out.println(timepoint);
+                    a.setCloudcover(data.get("timepoint").getAsInt());
+                    a.setSeeing(data.get("seeing").getAsInt());
+                    a.setTransparency(data.get("transparency").getAsInt());
+                    a.setLiftedIndex(data.get("lifted_index").getAsInt());
+                    a.setRh2m(data.get("rh2m").getAsInt());
+                    a.setWindDir(wind.get("direction").getAsString());
+                    a.setWindSpeed(wind.get("speed").getAsInt());
+                    a.setPrec(data.get("prec_type").getAsString());
+                    break;
+                }
+                case 6:{
+                    JsonObject wind = data.get("wind10m").getAsJsonObject();
+//                   System.out.println(timepoint);
+                   b.setCloudcover(data.get("timepoint").getAsInt());
+                   b.setSeeing(data.get("seeing").getAsInt());
+                   b.setTransparency(data.get("transparency").getAsInt());
+                   b.setLiftedIndex(data.get("lifted_index").getAsInt());
+                   b.setRh2m(data.get("rh2m").getAsInt());
+                   b.setWindDir(wind.get("direction").getAsString());
+                   b.setWindSpeed(wind.get("speed").getAsInt());
+                   b.setPrec(data.get("prec_type").getAsString());
+                    break;
+                }
+                case 9:{
+                    JsonObject wind = data.get("wind10m").getAsJsonObject();
+//                    System.out.println(timepoint);
+                    c.setCloudcover(data.get("timepoint").getAsInt());
+                    c.setSeeing(data.get("seeing").getAsInt());
+                    c.setTransparency(data.get("transparency").getAsInt());
+                    c.setLiftedIndex(data.get("lifted_index").getAsInt());
+                    c.setRh2m(data.get("rh2m").getAsInt());
+                    c.setWindDir(wind.get("direction").getAsString());
+                    c.setWindSpeed(wind.get("speed").getAsInt());
+                    c.setPrec(data.get("prec_type").getAsString());
+                    break;
+                }
+                case 12:{
+                    JsonObject wind = data.get("wind10m").getAsJsonObject();
+//                    System.out.println(timepoint);
+                    d.setCloudcover(data.get("timepoint").getAsInt());
+                    d.setSeeing(data.get("seeing").getAsInt());
+                    d.setTransparency(data.get("transparency").getAsInt());
+                    d.setLiftedIndex(data.get("lifted_index").getAsInt());
+                    d.setRh2m(data.get("rh2m").getAsInt());
+                    d.setWindDir(wind.get("direction").getAsString());
+                    d.setWindSpeed(wind.get("speed").getAsInt());
+                    d.setPrec(data.get("prec_type").getAsString());
+                    break;
+                }
+                case 15:{
+                    JsonObject wind = data.get("wind10m").getAsJsonObject();
+//                    System.out.println(timepoint);
+                    e.setCloudcover(data.get("timepoint").getAsInt());
+                    e.setSeeing(data.get("seeing").getAsInt());
+                    e.setTransparency(data.get("transparency").getAsInt());
+                    e.setLiftedIndex(data.get("lifted_index").getAsInt());
+                    e.setRh2m(data.get("rh2m").getAsInt());
+                    e.setWindDir(wind.get("direction").getAsString());
+                    e.setWindSpeed(wind.get("speed").getAsInt());
+                    e.setPrec(data.get("prec_type").getAsString());
+                }
+                case 18:{
+                    JsonObject wind = data.get("wind10m").getAsJsonObject();
+//                    System.out.println(timepoint);
+                    f.setCloudcover(data.get("timepoint").getAsInt());
+                    f.setSeeing(data.get("seeing").getAsInt());
+                    f.setTransparency(data.get("transparency").getAsInt());
+                    f.setLiftedIndex(data.get("lifted_index").getAsInt());
+                    f.setRh2m(data.get("rh2m").getAsInt());
+                    f.setWindDir(wind.get("direction").getAsString());
+                    f.setWindSpeed(wind.get("speed").getAsInt());
+                    f.setPrec(data.get("prec_type").getAsString());
+                }
+                case 21:{
+                    JsonObject wind = data.get("wind10m").getAsJsonObject();
+//                    System.out.println(timepoint);
+                    g.setCloudcover(data.get("timepoint").getAsInt());
+                    g.setSeeing(data.get("seeing").getAsInt());
+                    g.setTransparency(data.get("transparency").getAsInt());
+                    g.setLiftedIndex(data.get("lifted_index").getAsInt());
+                    g.setRh2m(data.get("rh2m").getAsInt());
+                    g.setWindDir(wind.get("direction").getAsString());
+                    g.setWindSpeed(wind.get("speed").getAsInt());
+                    g.setPrec(data.get("prec_type").getAsString());
+                }
+                case 24:{
+                    JsonObject wind = data.get("wind10m").getAsJsonObject();
+//                    System.out.println(timepoint);
+                    h.setCloudcover(data.get("timepoint").getAsInt());
+                    h.setSeeing(data.get("seeing").getAsInt());
+                    h.setTransparency(data.get("transparency").getAsInt());
+                    h.setLiftedIndex(data.get("lifted_index").getAsInt());
+                    h.setRh2m(data.get("rh2m").getAsInt());
+                    h.setWindDir(wind.get("direction").getAsString());
+                    h.setWindSpeed(wind.get("speed").getAsInt());
+                    h.setPrec(data.get("prec_type").getAsString());
                 }
             }
 
         }
+        System.out.println("YHIS"+a.getWindDir()+b.getWindDir()+c.getWindDir());
     }
 }
--- src/main/java/com/example/forecast_apk/forecastData.java
+++ src/main/java/com/example/forecast_apk/forecastData.java
@@ -1,5 +1,4 @@
 package com.example.forecast_apk;
-
 public class forecastData {
     private  int Cloudcover;
     private int seeing;
@@ -10,68 +9,54 @@ public class forecastData {
     private int windSpeed;
     private int temp2m;
     private String prec;
-    public forecastData(){}
+    public forecastData(){
 
+    }
     public int getSeeing() {
         return seeing;
     }
-
     public int getCloudcover() {
         return Cloudcover;
     }
-
     public int getTemp2m() {
         return temp2m;
     }
-
     public int getLiftedIndex() {
         return liftedIndex;
     }
-
     public int getRh2m() {
         return rh2m;
     }
-
     public int getTransparency() {
         return transparency;
     }
-
     public int getWindSpeed() {
         return windSpeed;
     }
-
     public String getPrec() {
         return prec;
     }
-
     public String getWindDir() {
         return windDir;
     }
-
     public void setCloudcover(int cloudcover) {
         Cloudcover = cloudcover;
     }
-
     public void setLiftedIndex(int liftedIndex) {
         this.liftedIndex = liftedIndex;
     }
-
     public void setPrec(String prec) {
         this.prec = prec;
     }
-
     public void setRh2m(int rh2m) {
         this.rh2m = rh2m;
     }
-
     public void setSeeing(int seeing) {
         this.seeing = seeing;
     }
-
     public void setTemp2m(int temp2m) {
         this.temp2m = temp2m;
     }
-
     public void setTransparency(int transparency) {
         this.transparency = transparency;
     }
@@ -79,9 +64,7 @@ public class forecastData {
     public void setWindDir(String windDir) {
         this.windDir = windDir;
     }
-
     public void setWindSpeed(int windSpeed) {
         this.windSpeed = windSpeed;
     }
-
 }
